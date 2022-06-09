1428bd67929b8386358400e7e1f353618439f5a8
gatwi <faith.murithi@student.moringaschool.com>
2022-06-09 08:04:14 +0300

    commit to master

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 2
+const num2 = 31
+const multiply=num1*num2
 
-// Write your code here
+function getRandom() {
+    return Math.random();
+}
+
+const random = Math.floor(Math.random()* 100) +1;
+
+var max (num)
